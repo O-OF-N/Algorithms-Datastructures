e3cc8d90f8c5f099dd024e22b742ac1cbe4224cc
Vinod Mohanan <O-OF-N@users.noreply.github.com>
2017-02-26 15:23:07 -0500

    Update binary_search.js

--- Sorting/binary_search.js
+++ Sorting/binary_search.js
@@ -1,6 +1,11 @@
 const binarySearch = (arr,val)=>{
-	if(arr.length<1) return null;
+	if(arr.length<1) return false;
+	if(arr.length === 1){
+		if(val === arr[0]) return true;
+		else return false;
+	} 
 	const mid = Math.floor(arr.length/2);
 	const midVal = arr[mid];
-	return (midVal === val)? val:(val < midVal)?binarySearch(arr.slice(0,mid),val): binarySearch(arr.slice(mid),val);
+	return (midVal === val)? true:(val < midVal)?binarySearch(arr.slice(0,mid),val): binarySearch(arr.slice(mid),val);
 }
+
